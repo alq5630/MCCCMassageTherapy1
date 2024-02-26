341f3af0f1b54eb50ad86d39514208182ebd485a
Ayesha <ayeshaqahash@gmail.com>
2024-02-26 13:15:13 -0800

    new edits to JS, html, and css

--- Home/script.js
+++ Home/script.js
@@ -39,6 +39,8 @@ window.onload = function() {
       sidebarContent += '<a href="#" id="chapter7Note">Chapter 7: Body Mechanics, Client Positioning, and Draping</a><br>';
       sidebarContent += '<a href="#" id="chapter9Note">Chapter 9: Standard Precautions, Contraindications, and Emergency Preparedness</a><br>';
       sidebarContent += '<h2 id="practiceQuizzesLink">Practice Quizzes</h2>';
+      sidebarContent += '<a href="#" id="directionalTermsPq">Directional Terms, Body Movements, and Planes of the Body</a><br>';
+      sidebarContent += '<a href="#" id="muscleConceptsPq">Muscle Concepts</a><br>';
       sidebarContent += '<a href="#" id="chapter3Pq">Chapter 3: Tools of the Trade</a><br>';
       sidebarContent += '<a href="#" id="chapter8Pq">Chapter 8: Massage Techniques</a><br>';
       sidebarContent += '<a href="#" id="chapter7Pq">Chapter 7: Body Mechanics, Client Positioning, and Draping</a><br>';
@@ -106,66 +108,76 @@ window.onload = function() {
       // Event Listeners for Handouts
       document.getElementById('massageTerminology').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vTNb6pRrRXlvhiBAbX-acrtwu3XSEhy75RH0ygnJobS29fKu84E-szQuaR0SL5bPlZxJATGUwQZrh6f/pub?embedded=true" frameborder="0" width="100%" height="600px"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vTNb6pRrRXlvhiBAbX-acrtwu3XSEhy75RH0ygnJobS29fKu84E-szQuaR0SL5bPlZxJATGUwQZrh6f/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('fiveStrokes').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vT1kdsm6jVbwfhaWY1Verf1gyqYHOqH9UgrJFdf1FU_CldFnnDWR0YBJCGsFhHz-w/pub?embedded=true" frameborder="0" width="100%" height="600px"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vT1kdsm6jVbwfhaWY1Verf1gyqYHOqH9UgrJFdf1FU_CldFnnDWR0YBJCGsFhHz-w/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('endangermentSites').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTI6Sc00BSkhS7mHx0pAlzMgXXrGCP5WfB40aMTf_gs5MlIqmQfnAGc905YmUtSYGbFVvmCCOl27jpT/pub?start=false&loop=false&delayms=3000" frameborder="0" width="100%" height="600px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTI6Sc00BSkhS7mHx0pAlzMgXXrGCP5WfB40aMTf_gs5MlIqmQfnAGc905YmUtSYGbFVvmCCOl27jpT/pub?start=false&loop=false&delayms=3000" frameborder="0" width="100%" height="6000px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
       });
   
   
       // Event listeners for Chapter Note Guides
       document.getElementById('directionalTerms').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vQbCEkRYLY83Y0P9DZNTP9tpG9_SXteP5UQO8xdoYBkM157N7vPCjkrQ4lYffMT_m2--ErAr8XxvoUd/pub?embedded=true" frameborder="0" width="100%" height="600px"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vQbCEkRYLY83Y0P9DZNTP9tpG9_SXteP5UQO8xdoYBkM157N7vPCjkrQ4lYffMT_m2--ErAr8XxvoUd/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('chapter3Note').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vQVskFFSSDjyl5TuZrcct0I1uLJAcMNYhw7GdIQZJL04GT02lVpkJbKFRgDH7Lw0bdYo5-M7BAYIw27/pub?embedded=true" frameborder="0" width="100%" height="600px"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vQVskFFSSDjyl5TuZrcct0I1uLJAcMNYhw7GdIQZJL04GT02lVpkJbKFRgDH7Lw0bdYo5-M7BAYIw27/pub?embedded=true" frameborder="0" width="100%" height="12000px"></iframe>');
       });
   
       document.getElementById('chapter8Note').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vRX11qNZkwFbYZD9AGCwgZvy0qigGZjP5PAfiu7fukwOCzmbYJBT5MpFm2tb_fSZ5Bn9FtdnaJEpReS/pub?embedded=true" frameborder="0" width="100%" height="600px"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vRX11qNZkwFbYZD9AGCwgZvy0qigGZjP5PAfiu7fukwOCzmbYJBT5MpFm2tb_fSZ5Bn9FtdnaJEpReS/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('chapter7Note').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vSZ5P3A9FFPaVhCRezVIGvsC7TJVCxcz3rG3iXKXE_KLAWla17ZTqBpJ16KBOxwCejuVWEVOpVs4qJ6/pub?embedded=true" frameborder="0" width="100%" height="600px"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vSZ5P3A9FFPaVhCRezVIGvsC7TJVCxcz3rG3iXKXE_KLAWla17ZTqBpJ16KBOxwCejuVWEVOpVs4qJ6/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('chapter9Note').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vSWbu2y22BTQOXuh4gwlTQOecu5MzGCgjJJTf9ivHYllNZmC-BBYZiC1ZGnCUDw_T8BLvc1fX4leuxQ/pub?embedded=true" frameborder="0" width="100%" height="600px"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vSWbu2y22BTQOXuh4gwlTQOecu5MzGCgjJJTf9ivHYllNZmC-BBYZiC1ZGnCUDw_T8BLvc1fX4leuxQ/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
   
       // Event listeners for Practice Quizzes
+      document.getElementById('directionalTermsPq').addEventListener('click', function(e) {
+        e.preventDefault();
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vRIOnn4_E_5SWYHt977LVbxFLSpJ48BbDDOTa4k_1UHN-_T5emAuNHl78sOw_dCU8bdtHw5LkZ0nT47/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
+      });
+  
+      document.getElementById('muscleConceptsPq').addEventListener('click', function(e) {
+        e.preventDefault();
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vQRVfjeHzoY1tj1Lp8W7GVUaS254umftgTxfGvL8Sj8wwNar_WP0i66S6HnCTue0DZwYYVD_HBKNJf5/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
+      });
+  
       document.getElementById('chapter3Pq').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQasClFopnaZ3qSPMDUgcNj3h5iFxIJI-8XZkXOLVccsytUdVPrfTQj9MSoqNl4gg/embed?start=true&loop=false&delayms=3000" frameborder="0" width="100%" height="600px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vRtSuNgvaKq4SKEvU42tIEBZeMM0CO06K4Mx8mC2xpXx9m2lymoU1WcH1cDzqhRtaLO720iiXsnRHrB/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('chapter8Pq').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ4mKVqRMZghmt-gd9HUXQ7iau_KIzmft5OjmK9cNS4byIBQBLdVCq9kWKc1Qcaug/embed?start=true&loop=false&delayms=3000" frameborder="0" width="100%" height="600px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vTGp-Y62PXPY1FKYpuBKl0K77GIOrovYMV63Io_NmJFisA5oeGhtTjrWX_V1EHADL7G2WNlBNHQUA-b/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('chapter7Pq').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSt0gUF7jULT5SEz-1oZkIZ3LRVDC9ed6NNhq64Ryy5RfjjpPHWTwRDLA2DfhZkiQ/embed?start=true&loop=false&delayms=3000" frameborder="0" width="100%" height="600px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vSc0wSFADrfvm3N4fDsdC7fU6_6ytZrU-CwOTBWb0cofXA0y1lk-Al_zmM12OwRAVPG0EbNS6Fxg8YV/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       document.getElementById('chapter9Pq').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vT9UgZi5opZST13raOnLqZDBKcZuhHRJJLfGIqePClI74R0b3XWq_MQ6Y7_x-kEpg/embed?start=true&loop=false&delayms=5000" frameborder="0" width="100%" height="600px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/document/d/e/2PACX-1vRuaYeXTjX0Ie7vCAC58TSDnFqMy_UketfT_kdOcKIRbfjPsyDkcOLNrnq_fNut33B1HrFD4ylQf_bS/pub?embedded=true" frameborder="0" width="100%" height="6000px"></iframe>');
       });
   
       //Theory Exam #2
@@ -230,3 +242,4 @@ window.onload = function() {
       document.querySelector('.content').innerHTML = content;
     }
   };
+  
